64b910f3f7c523b0dfca3acc954bcc5dfe2e11fa
jack <chujiming_1992@126.com>
2016-05-17 13:07:55 +0800

    some change

--- js/page.js
+++ js/page.js
@@ -1,17 +1,6 @@
 $(function(){
 	$('#H5').fullpage({
 		'sectionsColor':['#4DCBE3','#4DB7B5','#4DCBE3','#4DB7B5','#FFFFFF'],
-
-		anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-
-		afterLoad: function(anchorLink, index){
-			if (index == 1) {}
-			if (index == 2) {}
-			if (index == 3) {}
-			if (index == 4) {}
-			if (index == 5) {}
-		},
-
 	});
 });
 
